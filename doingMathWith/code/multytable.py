c20c9b604a3d46d7a342947c47fbaf26d51c10e4
ma <ma_yamaki@yahoo.com>
2019-07-26 16:06:32 +0900

    kkoko

---
+++ doingMathWith/code/multytable.py
@@ -0,0 +1,14 @@
+'''
+Multiplication tbable printer
+'''
+
+def multi_table(a):
+
+    for i in range(1, 11):
+            print('{0} x {1} = {2}'.format(a, i, a*i))
+
+
+if __name__ == '__main__':
+
+    a = input('Your Number Please: ')
+    multi_table(float(a))
