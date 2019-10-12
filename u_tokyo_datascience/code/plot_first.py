57125aaca262e152fd039bf6c9903d09099d63b0
ma <ma_yamaki@yahoo.com>
2019-10-12 20:51:24 +0900

    mm

--- u_tokyo_datascience/code/plot_first.py
+++ u_tokyo_datascience/code/plot_first.py
@@ -1,2 +1,10 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+student_math = pd.read_csv('../data/student-mat.csv')
 
+x = np.random.randn(1000)
+
+plt.plot(x, np.sin(x), 'o')
+plt.grid(True)
+plt.show()
